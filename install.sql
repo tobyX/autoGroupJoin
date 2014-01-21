b3a334e9ab16cb0566a8e633380013248083a30b
Tobias Friebel <t.friebel@alpha.de>
2014-01-21 15:48:18 +0100

    do not use complex sql

--- install.sql
+++ install.sql
@@ -1,3 +1,3 @@
-ALTER TABLE wcf1_user_group
-	ADD autoGroupJoinDays INT( 11 ) NULL DEFAULT NULL,
-	ADD autoGroupJoinActivityPoints INT( 11 ) NULL DEFAULT NULL;
+ALTER TABLE wcf1_user_group ADD autoGroupJoinDays INT( 11 ) NULL DEFAULT NULL;
+
+ALTER TABLE wcf1_user_group ADD autoGroupJoinActivityPoints INT( 11 ) NULL DEFAULT NULL;
